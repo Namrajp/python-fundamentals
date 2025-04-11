843ec223a18cdcec58b9d6acbebbfc8788cb30ff
NawarajPudasaini <active99raj@gmail.com>
2025-04-11 13:01:07 +1000

    Update function.py

--- function.py
+++ function.py
@@ -5,7 +5,7 @@ print(result)
 
 
 def max_num(num1, num2, num3):
-    if num1 >= num2 and num2 >= num3:
+    if num1 >= num2 and num1 >= num3:
         return num1
     elif num2 >= num3 and num2 >= num1:
         return num2
