b9a988254af4ac3b11db1e0879b0a6606024ad57
NawarajPudasaini <active99raj@gmail.com>
2025-11-03 22:44:22 +1100

    Implement basic arithmetic operations in main_script

---
+++ main_script.py
@@ -0,0 +1,5 @@
+import my_calculations
+result_add = my_calculations.add(10, 5)
+result_subtract = my_calculations.subtract(10, 5)
+print("Addition Result:", result_add)
+print("Subtraction Result:", result_subtract)
