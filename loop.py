a14bd28976e42c6653f7753cb2fd131adf7af3bd
NawarajPudasaini <active99raj@gmail.com>
2025-11-03 21:30:17 +1100

    Enhance loop.py with new loop structures
    Added additional loops for printing user names and counts.

--- loop.py
+++ loop.py
@@ -31,3 +31,19 @@ count =0
 while count < 10:
     print(f'count: {count}')
     count += 1
+y=1
+while y<=5:
+    print(y)
+    y+=1    
+
+for i in range(1,6):
+    print(f'count: {i}')
+
+users = ['Alice', 'Bob', 'Charlie', 'Diana']
+for user in users:
+    print(f'User: {user}')    
+
+print("Using index to access users:")
+
+for i in range((len(users))):
+    print(f'Index {i} User: {users[i]}')
